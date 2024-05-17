156df364a66f8c0337c71bb3456c0cef3a009d8b
Mukesh <mukeshrana90@gmail.com>
2024-05-17 09:46:39 +0530

    add item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-import Image from "next/image";
-
-export default function Home() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+const getData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
-  <main>
-    <p>welcome</p>
-  </main>
+    <main>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>User ID</TableHead>
+            <TableHead>Title</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data &&
+            data.map((item: any, index: number) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">{item?.id}</TableCell>
+                  <TableCell>{item?.userId}</TableCell>
+                  <TableCell>{item?.title}</TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </main>
   );
 }
